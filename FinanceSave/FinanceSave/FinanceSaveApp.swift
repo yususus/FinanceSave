0aa235dbc1950dcca43ea4827016834e65ca7742
Yusuf Aydın <yusayd3825@gmail.com>
2023-12-16 13:34:09 +0300

    created HomeView

---
+++ FinanceSave/FinanceSave/FinanceSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceSaveApp.swift
+//  FinanceSave
+//
+//  Created by yusuf on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FinanceSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceSave/FinanceSave/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  FinanceSave
+//
+//  Created by yusuf on 16.12.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
