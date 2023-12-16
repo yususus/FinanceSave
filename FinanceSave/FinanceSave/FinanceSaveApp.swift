f6c60eb6b91e561f9b7ef68a4ceb23bc3d9edb75
Yusuf Aydın <yusayd3825@gmail.com>
2023-12-16 13:40:27 +0300

    update PlusView

--- FinanceSave/FinanceSave/FinanceSaveApp.swift
+++ FinanceSave/FinanceSave/FinanceSaveApp.swift
@@ -10,8 +10,9 @@ import SwiftUI
 @main
 struct FinanceSaveApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+                WindowGroup {
+                    let dataManager = DataManager()
+                    HomeView(dataManager: dataManager)
+                }
+            }
 }
--- FinanceSave/FinanceSave/PlusView.swift
+++ FinanceSave/FinanceSave/PlusView.swift
@@ -8,8 +8,95 @@
 import SwiftUI
 
 struct PlusView: View {
+    @StateObject private var dataManager = DataManager()
+
+    func newFocus() {
+        if dataManager.itemtotal >= dataManager.hedef {
+            dataManager.itemtotal = 0
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Hedef: 5000")
+                    .font(.title)
+                CircleProgress(progress: dataManager.itemtargetProgress, total: dataManager.itemtotal)
+                    .frame(width: Const.width * 0.7, height: Const.height * 0.35)
+                Spacer()
+
+                // number input
+                TextField("Birikim Ekleyin ", text: $dataManager.userInput)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+                    .padding().frame(width: 250)
+
+                // save button
+                Button(action: {
+                    if let userValue = Double(dataManager.userInput) {
+                        dataManager.itemtotal += userValue
+                        dataManager.addToToplam(deger: userValue)
+                        dataManager.itemtargetProgress = dataManager.itemtotal / dataManager.hedef
+
+                        if dataManager.itemtotal >= dataManager.hedef && dataManager.itemtargetProgress >= 1 {
+                            dataManager.itemtotal = 0
+                        }
+
+                        dataManager.userInput = ""
+                        dataManager.saveData()
+                    }
+                    print("Kullanıcı girdisi: *******************")
+                    print("tebrikler tamamlandı")
+                    print("ilerleme yüzdesi: \(dataManager.itemtargetProgress)")
+                    print("toplam deger \(dataManager.itemtotal)")
+                }) {
+                    Text("Save")
+                        .padding()
+                        .font(.title3)
+                        .background(Color.mint)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                }
+            }
+            .navigationTitle("Biriktilen Tutar")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                dataManager.loadData()
+            }
+        }
+    }
+}
+
+struct CircleProgress: View {
+    let progress: Double
+    let total: Double
+
+    var body: some View {
+        ZStack {
+            Text("TOPLAM BİRİKİM: \(total, specifier: "%.2f")")
+                .frame(width: 150, alignment: .center)
+                .padding()
+                .overlay(
+                    Circle()
+                        .stroke(
+                            Color.red.opacity(0.3), lineWidth: 20
+                        )
+                        .frame(width: Const.width * 0.7, height: Const.height * 0.35)
+                )
+            // 2
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(
+                    Color.mint,
+                    style: StrokeStyle(
+                        lineWidth: 20,
+                        lineCap: .round,
+                        lineJoin: .miter
+                    )
+                )
+                .rotationEffect(.degrees(-90))
+                .animation(.spring, value: progress)
+        }
     }
 }
 
